ceacc5e9386233d7469d0062b2efd6e984b6c6e5
Maksim Morozov <maxpower656@gmail.com>
2024-03-06 18:47:41 +0300

    Add limit per day test

--- main_test.go
+++ main_test.go
@@ -61,7 +61,7 @@ func TestRewriteRouteOk(t *testing.T) {
 	assert.Equal(t, "You are a witty comedian known for sharing corny dad jokes.", w.Body.String())
 }
 
-func TestRewriteRouteOpenaiError(t *testing.T) {
+func TestContinueRouteOpenaiError(t *testing.T) {
 	limitPerDay = 1000
 	rst := getRestyClient()
 	httpmock.ActivateNonDefault(rst.GetClient())
@@ -88,9 +88,61 @@ func TestRewriteRouteOpenaiError(t *testing.T) {
 
 	jsonValue, _ := json.Marshal(values)
 
-	req, _ := http.NewRequest("POST", "/rewrite", bytes.NewBuffer(jsonValue))
+	req, _ := http.NewRequest("POST", "/continue", bytes.NewBuffer(jsonValue))
 	router.ServeHTTP(w, req)
 
 	assert.Equal(t, http.StatusServiceUnavailable, w.Code)
 	assert.Contains(t, w.Body.String(), "Service Unavailable")
+}
+
+func TestShortenRouteLimit(t *testing.T) {
+	limitPerDay = 1
+	timestamps = nil
+	// os.Remove(os.TempDir() + filename)
+	rst := getRestyClient()
+	httpmock.ActivateNonDefault(rst.GetClient())
+	defer httpmock.DeactivateAndReset()
+
+	httpmock.RegisterResponder(
+		"POST",
+		apiEndpoint,
+		httpmock.NewStringResponder(200, `{
+			"id": "chatcmpl-8yiKsPfAD4a9d0x1QBLh4kaCgg6lm",
+			"object": "chat.completion",
+			"created": 1709480798,
+			"model": "gpt-3.5-turbo-0125",
+			"choices": [
+			  {
+				"index": 0,
+				"message": {
+				  "role": "assistant",
+				  "content": "You are a witty comedian known for sharing corny dad jokes."
+				},
+				"logprobs": null,
+				"finish_reason": "stop"
+			  }
+			],
+			"usage": {
+			  "prompt_tokens": 26,
+			  "completion_tokens": 13,
+			  "total_tokens": 39
+			},
+			"system_fingerprint": "fp_2b778c6b35"
+		  }`),
+	)
+
+	router := setupRouter()
+
+	values := map[string]string{"text": "example text"}
+	jsonValue, _ := json.Marshal(values)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "/shorten", bytes.NewBuffer(jsonValue))
+	router.ServeHTTP(w, req)
+	assert.Equal(t, http.StatusOK, w.Code)
+
+	w = httptest.NewRecorder()
+	req, _ = http.NewRequest("POST", "/shorten", bytes.NewBuffer(jsonValue))
+	router.ServeHTTP(w, req)
+	assert.Equal(t, http.StatusTooManyRequests, w.Code)
 }
